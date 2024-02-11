91e78938ef492fbe74c896a5d50729be825f5c30
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2024-02-10 22:01:40 -0300

    feat: add service item component

---
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Service } from "@prisma/client";
+import Image from "next/image";
+
+interface ServiceItemProps {
+  service: Service;
+}
+
+const ServiceItem = ({ service }: ServiceItemProps) => {
+  return (
+    <Card>
+      <CardContent className="p-3">
+        <div className="flex gap-4 items-center">
+          <div className="relative size-[110px]">
+            <Image
+              className="object-contain rounded-lg"
+              src={service.imageUrl}
+              alt={service.name}
+              fill
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <h2 className="font-bold">{service.name}</h2>
+            <p className="text-sm text-gray-400">{service.description}</p>
+
+            <div className="flex items-center justify-between mt-3">
+              <p className="text-primary text-sm font-bold">
+                {Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(Number(service.price))}
+              </p>
+              <Button size="sm" variant="secondary">
+                Reservar
+              </Button>
+            </div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ServiceItem;
